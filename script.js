3e0bd38f8045fd23f1b0a248a72ab87dca8f636f
Giorgi Malakmadze <malakmadzegio5@gmail.com>
2024-01-11 19:54:10 +0400

    javascript yeah!

---
+++ script.js
@@ -0,0 +1,50 @@
+// var FirstNumber = 17;
+// var SecondNumber = 7;
+
+// var total = FirstNumber + SecondNumber
+
+// console.log(total)
+
+
+
+
+
+let combine1 = 32;
+let plus = "+";
+let combine2 = 17;
+let total = combine1 + combine2
+
+
+document.getElementById("combine1").innerHTML = combine1;
+document.getElementById("plus").innerHTML = plus;
+document.getElementById("combine2").innerHTML = combine2;
+document.getElementById("total").innerHTML = total;
+let one = 55;
+let plus2 ="+";
+let tow = 43;
+let total2 = one + tow
+document.getElementById("one").innerHTML = one;
+document.getElementById("plus2").innerHTML = plus2;
+document.getElementById("tow").innerHTML = tow;
+document.getElementById("total2").innerHTML = total2;
+
+let samravli = 3;
+let gamravleba = "*";
+let mamravli = 7;
+let namravli = samravli * mamravli
+
+document.getElementById("samravli").innerHTML = samravli;
+document.getElementById("gamravleba").innerHTML = gamravleba;
+document.getElementById("mamravli").innerHTML = mamravli;
+document.getElementById("namravli").innerHTML = namravli;
+
+let pudze = 3
+let pirveli = "?"
+let meore = 5
+let mesame = Math.cbrt(pudze)
+
+document.getElementById("pudze").innerHTML = pudze;
+document.getElementById("pirveli").innerHTML = pirveli;
+document.getElementById("meore").innerHTML = meore;
+document.getElementById("mesame").innerHTML = mesame;
+
